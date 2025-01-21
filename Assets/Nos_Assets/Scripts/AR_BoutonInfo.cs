c90da3ec74e77d2587d1957203479604de479004
Emery <119424953+EmeryDcs@users.noreply.github.com>
2025-01-21 16:51:39 +0100

    UPD : UI scan + ui info fleurs

--- Assets/Nos_Assets/Scripts/AR_BoutonInfo.cs
+++ Assets/Nos_Assets/Scripts/AR_BoutonInfo.cs
@@ -5,12 +5,15 @@ using TMPro;
 public class AR_BoutonInfo : MonoBehaviour
 {
     public FlowerScriptableObject flowerData;
-    public GameObject uIInfo;
     public TMP_Text uIInfoText;
 
     public void DisplayInfo()
     {
-        uIInfo.SetActive(true);
+        GameObject uIInfo = GameObject.Find("UI Info Fleur");
+
+        uIInfo.transform.GetChild(0).gameObject.SetActive(true);
+        uIInfo.transform.GetChild(1).gameObject.SetActive(true);
+
         uIInfoText.text = flowerData.description;
     }
 }
---
+++ Assets/Nos_Assets/Scripts/AR_DeactivateBorderPlaneDetected.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class AR_DeactivateBorderPlaneDetected : MonoBehaviour
+{
+    public Material materialTransparent;
+    public GameObject xROrigin;
+
+    public void DeactivateBorderPlaneDetected()
+    {
+        //on cherche tous les objets qui ont été trackés en tant que ARPlane
+        ARPlane[] gameObjects = FindObjectsByType<ARPlane>(FindObjectsSortMode.None);
+
+        foreach (ARPlane plane in gameObjects)
+        {
+            //on remplace la ligne noire par un matériau transparent
+            plane.gameObject.GetComponent<LineRenderer>().material = materialTransparent;
+            xROrigin.GetComponent<ARPlaneManager>().enabled = false;
+        }
+    }
+}
